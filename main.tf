fe38957e8cfaec1af5aaec4f953bd951dc7d0798
micedu <michel.gonzalez@gmail.com>
2025-05-30 19:47:06 -0500

    Add files via upload
    Actualización de archivos Terraform y Ansible para laboratorio de IaC

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 provider "proxmox" {
-  pm_api_url = "https://your-proxmox:8006/api2/json"
-  pm_user = "root@pam"
-  pm_password = "your-password"
+  pm_api_url = "https://192.168.100.10:8006/api2/json"
+  pm_user    = "root@pam"
+  pm_password = "your_password"
   pm_tls_insecure = true
 }
 
 resource "proxmox_lxc" "container" {
-  count = length(var.containers)
-
-  hostname = var.containers[count.index]["hostname"]
+  count = 3
+  hostname = ["web", "db", "monitor"][count.index]
   ostemplate = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
-  target_node = "proxmox"
-  cores = var.containers[count.index]["cores"]
-  memory = var.containers[count.index]["memory"]
+  target_node = "pve"
+  vmid = 100 + count.index
+  cores = 1
+  memory = 512
+  swap = 0
   unprivileged = true
-  network {
-    name = "eth0"
-    bridge = "vmbr0"
-    ip = var.containers[count.index]["ip"]
-  }
+  start = false
+  tty = 2
+  features = ""
+  net0 = "name=eth0,ip=192.168.100.10${count.index + 1}/24,bridge=vmbr0"
 }
