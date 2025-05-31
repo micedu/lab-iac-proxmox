bdd6df5f5da6bfa8b0ef5bfd8db3ed3c98e289ce
micedu <michel.gonzalez@gmail.com>
2025-05-30 20:20:27 -0500

    Add files via upload
    Update 2

--- main.tf
+++ main.tf
@@ -1,22 +1,16 @@
-provider "proxmox" {
-  pm_api_url = "https://192.168.100.10:8006/api2/json"
-  pm_user    = "root@pam"
-  pm_password = "your_password"
-  pm_tls_insecure = true
+
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "~> 2.9.11"
+    }
+  }
 }
 
-resource "proxmox_lxc" "container" {
-  count = 3
-  hostname = ["web", "db", "monitor"][count.index]
-  ostemplate = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
-  target_node = "pve"
-  vmid = 100 + count.index
-  cores = 1
-  memory = 512
-  swap = 0
-  unprivileged = true
-  start = false
-  tty = 2
-  features = ""
-  net0 = "name=eth0,ip=192.168.100.10${count.index + 1}/24,bridge=vmbr0"
+provider "proxmox" {
+  pm_api_url      = "https://192.168.100.10:8006/api2/json"
+  pm_user         = "root@pam"
+  pm_password     = "tu_clave_aqui"
+  pm_tls_insecure = true
 }
